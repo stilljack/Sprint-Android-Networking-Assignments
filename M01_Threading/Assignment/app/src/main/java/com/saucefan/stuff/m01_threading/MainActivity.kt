fa26f6960c550319771e1243c981092eb1dba098
jack seymour <stilljack@gmail.com>
2019-09-02 11:35:56 -0700

    so in part 3 9.b -- i can't refer to my MyasyncTask object (new) in onstop cause it's out of scope (I wonder if  I chose the right words for any of that.)
    I'll have to figure this out tomorrow when everybody is back in the game. stupid holidays.

--- M01_Threading/Assignment/app/src/main/java/com/saucefan/stuff/m01_threading/MainActivity.kt
+++ M01_Threading/Assignment/app/src/main/java/com/saucefan/stuff/m01_threading/MainActivity.kt
@@ -2,46 +2,24 @@ package com.saucefan.stuff.m01_threading
 
 import android.app.PendingIntent
 import android.content.Intent
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.logging.Logger.global
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        tv.visibility = View.GONE
 
-
-           progressBar.visibility = View.VISIBLE
-                //so that works but...
-           // visSwap()
-                //just calling the method that worked before doesn't... weird...
-
-        val primeNumbers = primes().take(16000).joinToString(", ")
-        tv.text = "Primes: $primeNumbers"
-        tv.visibility=View.VISIBLE
-        visSwap()
-
-
-        //just cause i didn't believe this code would work
-       // tv.setOnClickListener{
-      //      visSwap()
-      //  }
-    }
-
-
-
-    //
-    fun visSwap(){
-        if (progressBar.visibility == View.GONE){
-            progressBar.visibility = View.VISIBLE
+    fun visSwap(view:View){
+        if (view.visibility == View.INVISIBLE){
+            view.visibility = View.VISIBLE
         }
-        else progressBar.visibility = View.GONE
+        else view.visibility = View.INVISIBLE
     }
     fun primes(): Sequence<Long> {
         var i = 0L
@@ -51,5 +29,66 @@ class MainActivity : AppCompatActivity() {
                 .forEach { yield(it) }
         }
     }
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val textView=findViewById<TextView>(R.id.tv)
+        val progressBar =findViewById<ProgressBar>(R.id.progressBar)
+
+        class MyAsyncTask : AsyncTask<Unit, Int, String>() {
+            override fun doInBackground(vararg p0: Unit?): String {
+                val primeNumbers = primes().take(1600).joinToString(", ")
+                return primeNumbers
+            }
+
+            override fun onPostExecute(result: String?) {
+                super.onPostExecute(result)
+                visSwap(progressBar)
+                visSwap(tv)
+                tv.text = result
+            }
+
+            override fun onPreExecute() {
+                super.onPreExecute()
+                visSwap(tv)
+                visSwap(progressBar)
+            }
+
+            override fun onCancelled() {
+                super.onCancelled()
+            }
+
+        }
+
+           //progressBar.visibility = View.VISIBLE
+                //so that works but...
+           // visSwap()
+                //just calling the method that worked before doesn't... weird...
+
+       //
+       // tv.text = "Primes: $primeNumbers"
+       // tv.visibility=View.VISIBLE
+       // visSwap()
+
+
+        //just cause i didn't believe this code would work
+       // tv.setOnClickListener{
+      //      visSwap()
+      //  }
+        val new = MyAsyncTask()
+
+        btn.setOnClickListener {
+            new.execute()
+        }
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+
+    //
 
 }

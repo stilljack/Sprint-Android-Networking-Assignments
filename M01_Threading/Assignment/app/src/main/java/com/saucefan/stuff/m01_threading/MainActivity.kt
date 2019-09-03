cd209cd87c3a8e3714f052470da890478be8dbd3
jack seymour <stilljack@gmail.com>
2019-09-02 20:34:31 -0700

    so afaict from reading about this and trying stuff, we need an interuptable loop in doinbackground which -- i'm just not clear on how to suss out the wildly unclear method to find primes.. I'm sure this stuff will be covered tomorrow...

--- M01_Threading/Assignment/app/src/main/java/com/saucefan/stuff/m01_threading/MainActivity.kt
+++ M01_Threading/Assignment/app/src/main/java/com/saucefan/stuff/m01_threading/MainActivity.kt
@@ -3,85 +3,75 @@ package com.saucefan.stuff.m01_threading
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.ContactsContract
 import android.util.Log
 import android.view.View
-import android.widget.ProgressBar
-import android.widget.TextView
-import androidx.lifecycle.ViewModelProvider
+import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity() : AppCompatActivity() {
-lateinit var new:AsyncTask<Unit, Int, String>
-     fun visSwap(view:View){
-        if (view.visibility == View.INVISIBLE){
-            view.visibility = View.VISIBLE
+class MainActivity : AppCompatActivity() {
+    val newasync = MyAsyncTask()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn.setOnClickListener {
+            newasync.execute()
         }
-        else view.visibility = View.INVISIBLE
+
+        btn2.setOnClickListener {
+          newasync.cancel(true)
+        }
     }
+
+    inner class MyAsyncTask: AsyncTask<Unit, Int, String>(){
+
+        override fun onPreExecute() {
+            visSwap(tv)
+            visSwap(progressBar)
+            super.onPreExecute()
+        }
+
+        override fun doInBackground(vararg p0: Unit?): String {
+            val primeNumbers = primes().take(1600).joinToString(", ")
+            return primeNumbers
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            visSwap(tv)
+            visSwap(progressBar)
+            tv.text = result
+        }
+
+        override fun onCancelled() {
+            super.onCancelled()
+            Log.i("oncanceled", isCancelled.toString())
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("oncanceled", "Activity Stopped")
+        newasync.cancel(true)
+    }
+
     fun primes(): Sequence<Long> {
         var i = 0L
         return sequence {
             generateSequence { i++ }
                 .filter { n -> n > 1 && ((2 until n).none { i -> n % i == 0L }) }
                 .forEach { yield(it) }
-
         }
     }
-    override fun onCreate(savedInstanceState: Bundle?) {
 
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val textView=findViewById<TextView>(R.id.tv)
-        val progressBar =findViewById<ProgressBar>(R.id.progressBar)
-
-        class MyAsyncTask : AsyncTask<Unit, Int, String>() {
-            override fun doInBackground(vararg p0: Unit?): String {
-                val primeNumbers = primes().take(16000).joinToString(", ")
-                return primeNumbers
-
+    fun visSwap(view:View) {
+        if(view.isVisible==false){
+            view.isVisible = true
             }
-
-            override fun onPostExecute(result: String?) {
-                super.onPostExecute(result)
-                visSwap(progressBar)
-                visSwap(tv)
-                tv.text = result
-                if (isCancelled()){
-                    Log.i("stopped","onpostexecute")
-                }
+        else {
+            view.isVisible = false
             }
-
-            override fun onPreExecute() {
-                super.onPreExecute()
-                visSwap(tv)
-                visSwap(progressBar)
-            }
-
-            override fun onCancelled() {
-                super.onCancelled()
-                Log.i("stoped","oncanceled")
-            }
-
         }
-
-
-        val new = MyAsyncTask()
-
-        btn.setOnClickListener {
-            new.execute()
-
-        }
-
-    }
-
-    override fun onStop() {
-
-        new.cancel(true)
-        super.onStop()
-    }
-
-
-    //
-
 }
+

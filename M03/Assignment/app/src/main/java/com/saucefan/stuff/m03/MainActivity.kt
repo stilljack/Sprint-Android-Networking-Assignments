96c7d4860955c7f0accae15f767fe7d02698a71d
jack seymour <stilljack@gmail.com>
2019-09-04 14:09:00 -0700

    intent structure complete

--- M03/Assignment/app/src/main/java/com/saucefan/stuff/m03/MainActivity.kt
+++ M03/Assignment/app/src/main/java/com/saucefan/stuff/m03/MainActivity.kt
@@ -8,17 +8,20 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
+import com.saucefan.stuff.m03.allcapsactivities.DeleteActivity
+import com.saucefan.stuff.m03.allcapsactivities.PostAcitivty
+import com.saucefan.stuff.m03.allcapsactivities.PutActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.internal.waitMillis
 
 class MainActivity : AppCompatActivity() {
-    private fun isNetworkConnected(){
+    private fun isNetworkConnected():Boolean{
         val connectivityManager =
             getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         val networkInfo = connectivityManager.activeNetworkInfo
         Toast.makeText(this,"is it connected? ${networkInfo.isConnected}",Toast.LENGTH_SHORT).show()
-       if (!networkInfo.isConnected){
-           Toast.makeText(this,"is it connected? false",Toast.LENGTH_SHORT).show()
-        }
+
+        return networkInfo.isConnected
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,19 +29,35 @@ class MainActivity : AppCompatActivity() {
         setTitle("I didn't know this was a thing, neat")
 
         GetButton.setOnClickListener {
-            isNetworkConnected()
-            startActivity(Intent(this@MainActivity, HttpDeleteActivity::class.java))
 
-
-
+            if (!isNetworkConnected()){
+                Toast.makeText(this,"is it connected? false",Toast.LENGTH_SHORT).show()
+            } else
+            startActivity(Intent(this, GetPickerActivity::class.java))
         }
 
         PutButton.setOnClickListener {
+            if (!isNetworkConnected()){
+                Toast.makeText(this,"is it connected? false",Toast.LENGTH_SHORT).show()
+            } else
+            startActivity(Intent(this, PutActivity::class.java))
         }
 
-        DeleteButton.setOnClickListener {  }
+        DeleteButton.setOnClickListener {
+            if (!isNetworkConnected()){
+                Toast.makeText(this,"is it connected? false",Toast.LENGTH_SHORT).show()
+            } else
+            startActivity(Intent(this, DeleteActivity::class.java))
+        }
 
-        PostButton.setOnClickListener {  }
+        PostButton.setOnClickListener {
+            if (!isNetworkConnected()){
+                Toast.makeText(this,"is it connected? false",Toast.LENGTH_SHORT).show()
+            } else
+            startActivity(Intent(this, PostAcitivty::class.java))
+        }
+
+
 
 
     }
--- M03/Assignment/app/src/main/java/com/saucefan/stuff/m03/allcapsactivities/GetActivity.kt
+++ M03/Assignment/app/src/main/java/com/saucefan/stuff/m03/allcapsactivities/GetActivity.kt
@@ -2,6 +2,7 @@ package com.saucefan.stuff.m03.allcapsactivities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.saucefan.stuff.m03.R
 
 class GetActivity : AppCompatActivity() {
@@ -9,5 +10,13 @@ class GetActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_get)
+
+        val type = intent.getStringExtra("get")
+        when (type){
+            "path" -> Toast.makeText(this,"path", Toast.LENGTH_SHORT).show()
+             "query" -> Toast.makeText(this,"query",Toast.LENGTH_SHORT).show()
+              "get" -> Toast.makeText(this,"get",Toast.LENGTH_SHORT).show()
+
+        }
     }
 }

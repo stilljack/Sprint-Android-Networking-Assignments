1ae840e5365806487fc91c8aedd095adc0f78788
jack seymour <stilljack@gmail.com>
2019-09-04 13:01:24 -0700

    permissions, imports, xml checks connection and toasts result on first button press for now, connected function will continue to exist.
    TODO: find out why this means of finding out the status of the connection is depreciated.

--- M03/Assignment/app/src/main/java/com/saucefan/stuff/m03/MainActivity.kt
+++ M03/Assignment/app/src/main/java/com/saucefan/stuff/m03/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.saucefan.stuff.m03
 
+import android.content.Context
+import android.net.ConnectivityManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+    private fun isNetworkConnected(): Boolean {
+        val connectivityManager =
+            getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkInfo = connectivityManager.activeNetworkInfo
+        Toast.makeText(this,"is it connected? ${networkInfo.isConnected}",Toast.LENGTH_SHORT).show()
+        return networkInfo?.isConnected == true
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        GetButton.setOnClickListener {
+            isNetworkConnected()
+
+        }
+
+
     }
 }
---
+++ M03/Assignment/app/src/main/java/com/saucefan/stuff/m03/allcapsactivities/GetActivity.kt
@@ -0,0 +1,13 @@
+package com.saucefan.stuff.m03.allcapsactivities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.saucefan.stuff.m03.R
+
+class GetActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_get)
+    }
+}

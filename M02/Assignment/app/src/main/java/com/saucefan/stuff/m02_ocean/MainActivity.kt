9f68acae5bd9b2a209da5235d6d2d9cb94368555
jack seymour <stilljack@gmail.com>
2019-09-04 16:56:25 -0700

    back to this after i eat a giant fing steak

--- M02/Assignment/app/src/main/java/com/saucefan/stuff/m02_ocean/MainActivity.kt
+++ M02/Assignment/app/src/main/java/com/saucefan/stuff/m02_ocean/MainActivity.kt
@@ -4,21 +4,28 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.recyclerview.widget.GridLayoutManager
 import com.saucefan.stuff.m02_ocean.api.OceaniaCountriesRetriever
-import com.saucefan.stuff.m02_ocean.model.OceaniaCountryList
 import com.saucefan.stuff.m02_ocean.model.wtf
 import kotlinx.android.synthetic.main.activity_main.*
-
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import com.saucefan.stuff.m02_ocean.recycleview.Adapter
 
 class MainActivity : AppCompatActivity(), Callback<List<wtf>> {
+    private lateinit var layoutManager: GridLayoutManager
+    private lateinit var adapter: Adapter
+    private lateinit var mutlist:MutableList<wtf>
+
+
     override fun onFailure(call: Call<List<wtf>>, t: Throwable) {
         t.printStackTrace()
         val response = "faliure; ${t.message}"
         Toast.makeText(this@MainActivity, response, Toast.LENGTH_SHORT).show()
     }
+
+
 
     override fun onResponse(call: Call<List<wtf>>, response: Response<List<wtf>>) {
         if (response.isSuccessful) {
@@ -26,9 +33,13 @@ class MainActivity : AppCompatActivity(), Callback<List<wtf>> {
             var oceanMan = ""
             for (i in 0 until  oceaniaCountryList?.size as Int) {
                 oceanMan += oceaniaCountryList[i].name
+                mutlist.add(oceaniaCountryList[i])
             }
             Log.i("the plane from lost?","$oceaniaCountryList")
             tv_ctv.text = oceanMan
+            adapter=Adapter(mutlist)
+            layoutManager = GridLayoutManager(this, mutlist.size)
+            recycleView.layoutManager = layoutManager
         } else {
             val response = "response not successful; ${response.errorBody().toString()}"
             Toast.makeText(this@MainActivity, response, Toast.LENGTH_SHORT).show()

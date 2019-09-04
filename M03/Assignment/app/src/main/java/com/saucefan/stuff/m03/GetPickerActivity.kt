7fa5cb61e9ce0fde0ce39171b451ece62c115ed8
jack seymour <stilljack@gmail.com>
2019-09-04 12:30:51 -0700

    stol some imports from the morning -- couldn't get the material.button to work... hmm gonna see what thats all about

---
+++ M03/Assignment/app/src/main/java/com/saucefan/stuff/m03/GetPickerActivity.kt
@@ -0,0 +1,12 @@
+package com.saucefan.stuff.m03
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GetPickerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_get_picker)
+    }
+}
